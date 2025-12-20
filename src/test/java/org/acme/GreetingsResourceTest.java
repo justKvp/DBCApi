6308273b36e7dcddfd2d8768b4890daac08da633
justKvp <kvipka90@yandex.ru>
2025-12-20 21:01:29 +0300

    temporary remove testcontainers

--- src/test/java/org/acme/GreetingsResourceTest.java
+++ src/test/java/org/acme/GreetingsResourceTest.java
@@ -1,15 +1,12 @@
 package org.acme;
 
-import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
-import org.acme.fixture.DBFixture;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-//@QuarkusTestResource(DBFixture.class)
 class GreetingsResourceTest {
     @Test
     void testHelloEndpoint() {
